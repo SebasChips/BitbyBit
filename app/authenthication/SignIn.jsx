055e2c42801128262fa1314940f1b94e3f09db9f
SebasChips <sebastianalarconbejar@gmail.com>
2025-05-05 13:54:38 -0600

    signin google in web working & alerts change to toast

--- app/authenthication/SignIn.jsx
+++ app/authenthication/SignIn.jsx
@@ -1,37 +1,81 @@
-// ./authenthication/signin.js
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Text, TextInput, TouchableOpacity, Image } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import { auth } from "../../firebase/firebaseConfig.jsx";
+import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithCredential } from "firebase/auth";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import { makeRedirectUri } from "expo-auth-session";
+import { useNavigation } from "@react-navigation/native";
+import { colors, spacing, textStyles, formStyles, buttonStyles} from "./styles";
+import Toast from 'react-native-toast-message';
+
+
 
 const SignIn = () => {
-  const con =
-  return (
-    <View>
-    <Text>Registrarse</Text>
-    <TextInput
-      placeholder="Correo electrónico"
-      value={email}
-      onChangeText={setEmail}
-      autoCapitalize="none"
-      style={{ width: '80%', margin: 10, padding: 10, borderWidth: 1 }}
-    />
+  const [text, onChangeText] = React.useState('Useless Text');
+  const [number, onChangeNumber] = React.useState('');
+  const navigation = useNavigation();
 
-    <TextInput
-        placeholder="Contraseña"
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-        style={{ width: '80%', margin: 10, padding: 10, borderWidth: 1 }}
-    />
 
-    <TextInput
-        placeholder="Usuario"
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-        style={{ width: '80%', margin: 10, padding: 10, borderWidth: 1 }}
-    />
-    </View>
+  // Construye un redirectUri compatible con Expo Proxy para móvil
+    const redirectUri = makeRedirectUri({ useProxy: true });
+  
+    const [request, response, promptAsync] = Google.useAuthRequest({
+      webClientId: "61966159852-30er87tn5uojd5l0p8ndhriu144tpuj0.apps.googleusercontent.com",
+      expoClientId: "61966159852-jp4u85h56v7f36gnf1mq8lqn1u70gh24.apps.googleusercontent.com",
+      androidClientId: "61966159852-jp4u85h56v7f36gnf1mq8lqn1u70gh24.apps.googleusercontent.com",
+      iosClientId: "61966159852-bk80mn0a9pfuitkj1i8qv0f4kqtug8nu.apps.googleusercontent.com",
+      redirectUri,
+      scopes: ["openid", "profile", "email"],
+    });
+  
     
+      useEffect(() => {
+        if (response?.type === "success") {
+          const { id_token, access_token } = response.params;
+          const credential = GoogleAuthProvider.credential(id_token, access_token);
+          signInWithCredential(auth, credential)
+            .then(() => {
+              navigation.navigate("main");
+            })
+            .catch((error) => {
+              console.error("Error en signInWithCredential:", error);
+            });
+        }
+      }, [response]);
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <TextInput
+          onChangeText={onChangeText}
+          placeholder="Correo electrónico"
+        />
+        <TextInput
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="Contraseña"
+        />
+
+        <TouchableOpacity
+                //onPress={() => handleLogin(0)}
+                style={[buttonStyles.primary, { marginTop: spacing.large }]}
+              >
+                <Text style={{ color: "white", textAlign: "center" }}>
+                  Registrarse
+                </Text>
+              </TouchableOpacity>
+
+
+         <TouchableOpacity onPress={() => promptAsync({ useProxy: true })} disabled={!request}>
+          <Image
+            style={formStyles.googleButton}
+            source={require("../../assets/images/logo_google.png")}
+          />
+        </TouchableOpacity>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 
--- app/index.js
+++ app/index.js
@@ -3,28 +3,20 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignIn from './authenthication/SignIn';
 import login from './authenthication/login';
 import main from './profile/main';
+import Toast from 'react-native-toast-message';
 
 
 const Stack = createNativeStackNavigator();
 
 export default function index() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="login">
-      <Stack.Screen 
-        name="login" 
-        component={login}   
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen 
-          name="SignIn" 
-          component={SignIn} 
-          options={{ headerShown: false }}
-        />
-         <Stack.Screen 
-          name="main" 
-          component={main} 
-          options={{ headerShown: false }}
-        />
-    </Stack.Navigator>
+    <>
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="login">
+        <Stack.Screen name="login" component={login} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="main" component={main} />
+      </Stack.Navigator>
+      <Toast />
+    </>
   );
 }
