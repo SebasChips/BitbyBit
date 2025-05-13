0b2fde777bbfc546cfdafaafd1cc3ab449028307
IsiGOD <isidromartinezcavazos@gmail.com>
2025-05-13 13:54:14 -0600

    soy isidro

--- app/views/profile/main.jsx
+++ app/views/profile/main.jsx
@@ -3,7 +3,7 @@ import { View, TextInput, Text, Image, TouchableOpacity } from "react-native";
 const main =  () => {
   return (
     <View>
-      <Text >Main</Text>
+      <Text >Soy isidro</Text>
     </View>
   );
 };
