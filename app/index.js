44d35284a523a17b1f85cc2da5c46b0088441004
unknown <charlymj21@gmail.com>
2025-05-27 22:25:53 -0600

    update si

--- app/index.js
+++ app/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // Screens
-import SignIn from './screens/authenthication/SignIn';
-import Login from './screens/authenthication/Login';
+import SignIn from './views/authenthication/SignIn';
+import Login from './views/authenthication/Login';
 
 import FirstTimeRegister from './screens/profile/FirstTimeRegister';
 import Main from './screens/profile/Main';
 
-import FirstGame from './screens/lessons/lesson1/L1FirstGame';
-import SecondGame from './screens/lessons/lesson1/L1SecondGame';
-import ThirdGame from './screens/lessons/lesson1/L1ThirdGame';
+import FirstGame from './views/lessons/lesson1/L1FirstGame';
+import SecondGame from './views/lessons/lesson1/L1SecondGame';
+import ThirdGame from './views/lessons/lesson1/L1ThirdGame';
 
 import Toast from 'react-native-toast-message';
 
@@ -19,7 +19,7 @@ const Stack = createNativeStackNavigator();
 export default function index() {
   return (
     <>
-      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Main">
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Login">
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="SignIn" component={SignIn} />
         <Stack.Screen name="Main" component={Main} />
